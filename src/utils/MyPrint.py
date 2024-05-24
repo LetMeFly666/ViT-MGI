047aed3db3d8790b0d4d343164348395b35161d1
LetMeFly666 <814114971@qq.com>
2024-05-24 10:45:12 +0800

    chore: finish the LetPrinter
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- src/utils/MyPrint.py
+++ src/utils/MyPrint.py
@@ -2,6 +2,7 @@ from typing import Any
 from os import makedirs
 import builtins
 import inspect
+import tempfile
 
 
 builtins.stdout = print
@@ -17,15 +18,41 @@ class MyPrint:
         self.fileout = open(f'{dirname}/stdout.txt', 'w')
         
     def __call__(self, *args: Any, **kwds: Any) -> None:
-        builtins.stdout(*args, **kwds)
-        kwds['file'] = self.fileout
+        # get filename and lineNum
         stack = inspect.stack()
         frame = stack[2]
         filename = frame.filename
         lineno = frame.lineno
-        builtins.stdout(f'---------------------- {filename}:{lineno} [Begin] --------------------', file=self.fileout)
-        builtins.stdout(*args, **kwds)
-        builtins.stdout(f'---------------------- {filename}:{lineno} [End] ----------------------', file=self.fileout)
+        # write into tempfile
+        with tempfile.TemporaryFile(mode='w+', encoding='utf-8') as f:
+            originalFile = kwds.get('file', None)
+            kwds['file'] = f
+            builtins.stdout(*args, **kwds)
+            f.seek(0)
+            content = f.read()
+        # get data shape by tempfile
+        lines = content.splitlines()
+        if len(lines) > 1 and lines[-1] == '\n':
+            lines.pop()
+        length = max(len(line) + 4 for line in lines)
+        title = f'{filename}:{lineno}'
+        length = max(length, len(title) + 6)  # 第一行最短为 +- /path/to/file:2 -+
+        if (length - len(title)) % 2:  # 第一行左右要对称
+            length += 1
+        hyphen = '-' * ((length - len(title) - 4) // 2)
+        toShow = f'+{hyphen} {title} {hyphen}+\n'
+        for line in lines:
+            toShow += f'| {line}' + (' ' * (length - 3 - len(line))) + '|\n'
+        toShow += '+' + ('-' * (length - 2)) + '+\n'
+        # original print
+        if originalFile:
+            kwds['file'] = originalFile
+        else:
+            del kwds['file']
+        builtins.stdout(toShow, **kwds)
+        # write into file
+        kwds['file'] = self.fileout
+        builtins.stdout(toShow, **kwds)
         self.fileout.flush()
 
     def __del__(self) -> None:
