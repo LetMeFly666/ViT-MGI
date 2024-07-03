47b82746c7c127ec352697a8659f88c4a887895b
LetMeFly666 <814114971@qq.com>
2024-07-03 14:27:19 +0800

    train: 可训但loss为None且未统计准确率
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- main.py
+++ main.py
@@ -2,98 +2,173 @@
 Author: LetMeFly
 Date: 2024-07-03 10:37:25
 LastEditors: LetMeFly
-LastEditTime: 2024-07-03 10:50:47
+LastEditTime: 2024-07-03 11:16:31
 '''
 import datetime
-now = datetime.datetime.now().strftime('%Y.%m.%d-%H:%M:%S')
-del datetime
+getNow = lambda: datetime.datetime.now().strftime('%Y.%m.%d-%H:%M:%S')
+now = getNow()
+# del datetime
 from src.utils import initPrint
 from typing import List, Optional
 
+initPrint(now)
+print(now)
 
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, Subset
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from transformers import ViTForImageClassification, ViTConfig
 import numpy as np
+import random
 
-class FederatedModel:
-    def __init__(self, input_size, output_size):
-        self.weights = np.random.randn(input_size, output_size)
-    
-    def predict(self, x):
-        return np.dot(x, self.weights)
-    
-    def update_weights(self, grad, learning_rate):
-        self.weights -= learning_rate * grad
 
+# 定义ViT模型
+class ViTModel(nn.Module):
+    def __init__(self, num_classes=10):
+        super(ViTModel, self).__init__()
+        model_path = './data/vit_base_patch16_224'
+        config = ViTConfig.from_pretrained(model_path)
+        self.model = ViTForImageClassification.from_pretrained(model_path, config=config)
+        self.model.classifier = nn.Linear(self.model.config.hidden_size, num_classes)
+    
+    def forward(self, x):
+        return self.model(x).logits
+
+# 客户端类
 class Client:
-    def __init__(self, data, labels):
-        self.data = data
-        self.labels = labels
-        self.model: Optional[FederatedModel] = None
+    def __init__(self, data_loader: DataLoader):
+        self.data_loader = data_loader
+        self.model: Optional[ViTModel] = None
     
     def set_model(self, model):
         self.model = model
     
-    def compute_gradient(self):
-        predictions = self.model.predict(self.data)
-        grad = np.dot(self.data.T, (predictions - self.labels)) / len(self.labels)
-        return grad
+    def compute_gradient(self, criterion, device):
+        self.model.to(device)
+        self.model.train()
+        optimizer = optim.SGD(self.model.parameters(), lr=0.01, momentum=0.9)
+        optimizer.zero_grad()
+        
+        total_loss = 0.0
+        for images, labels in self.data_loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = self.model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            total_loss += loss.item()
+        
+        grads = []
+        for param in self.model.parameters():
+            grads.append(param.grad.clone().cpu())
+        
+        return grads, total_loss / len(self.data_loader)
 
+# 服务器类
 class Server:
-    def __init__(self, input_size, output_size, learning_rate):
-        self.global_model = FederatedModel(input_size, output_size)
-        self.learning_rate = learning_rate
+    def __init__(self, model, device):
+        self.global_model = model
+        self.device = device
     
     def distribute_model(self, clients: List[Client]):
         for client in clients:
             client.set_model(self.global_model)
     
-    def aggregate_gradients(self, grads):
-        return np.mean(grads, axis=0)
+    def aggregate_gradients(self, grads_list):
+        avg_grads = []
+        for grads in zip(*grads_list):
+            avg_grads.append(sum(grads) / len(grads))
+        return avg_grads
     
     def update_model(self, grads):
-        aggregated_grad = self.aggregate_gradients(grads)
-        self.global_model.update_weights(aggregated_grad, self.learning_rate)
+        for param, grad in zip(self.global_model.parameters(), grads):
+            param.grad = grad.to(self.device)
+        optimizer = optim.SGD(self.global_model.parameters(), lr=0.01, momentum=0.9)
+        optimizer.step()
 
-def generate_data(num_samples, input_size, output_size):
-    data = np.random.randn(num_samples, input_size)
-    labels = np.dot(data, np.random.randn(input_size, output_size))
-    return data, labels
-
-# Parameters
-num_clients = 5
-input_size = 10
-output_size = 1
-learning_rate = 0.01
-num_rounds = 10
-
-if __name__ == "__main__":
-    initPrint(now)
-    print(now)
-
-# Generate data for clients
-clients: List[Client] = []
-for _ in range(num_clients):
-    data, labels = generate_data(100, input_size, output_size)
-    clients.append(Client(data, labels))
-
-# Initialize server
-server = Server(input_size, output_size, learning_rate)
-
-# Federated learning process
-for round_num in range(num_rounds):
-    print(f"Round {round_num+1}")
+def get_data_loaders(num_clients, batch_size) -> List[Client]:
+    transform = transforms.Compose([
+        transforms.Resize(224),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
     
-    # Distribute the current global model to all clients
+    train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    
+    # 将数据集划分给多个客户端
+    dataset_size = len(train_dataset)
+    indices = list(range(dataset_size))
+    random.shuffle(indices)
+    split_indices = np.array_split(indices, num_clients)
+    
+    clients = []
+    for split in split_indices:
+        subset = Subset(train_dataset, split)
+        data_loader = DataLoader(subset, batch_size=batch_size, shuffle=True)
+        clients.append(Client(data_loader))
+    
+    return clients
+
+# 参数
+num_clients = 5
+batch_size = 32
+num_rounds = 10
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# 获取数据加载器
+clients = get_data_loaders(num_clients, batch_size)
+
+# 初始化服务器
+global_model = ViTModel()
+server = Server(global_model, device)
+
+# 联邦学习过程
+criterion = nn.CrossEntropyLoss()
+
+
+class TimeRecorder():
+    def __init__(self, eventName: str, eventTime: str):
+        self.eventName = eventName
+        self.eventTime = eventTime
+    
+    def __str__(self):
+        return f'{self.eventName} - {self.eventTime}'
+    
+
+timeList = []
+
+def printTimeList():
+    toPrint = 'TimeList:'
+    for i in range(len(timeList)):
+        toPrint += f'\n{i:02d}: {timeList[i]}'
+    print(toPrint)
+
+
+for round_num in range(num_rounds):
+    print(f"Round {round_num+1} of {num_rounds} | {getNow()}")
+    
+    # 分发当前的全局模型给所有客户端
     server.distribute_model(clients)
     
-    # Each client computes the gradient
-    gradients = []
+    # 每个客户端计算梯度
+    grads_list = []
+    total_loss = 0.0
     for th, client in enumerate(clients):
-        print(f'Client{th} computing gradient...')
-        grad = client.compute_gradient()
-        gradients.append(grad)
+        print(f'Client {th} is computing gradients... | {getNow()}')
+        grads, loss = client.compute_gradient(criterion, device)
+        print(f'Client {th} has computed gradients. | {getNow()}')
+        grads_list.append(grads)
+        total_loss += loss
     
-    # Server aggregates the gradients and updates the global model
-    server.update_model(gradients)
+    avg_loss = total_loss / num_clients
+    print(f"Average loss: {avg_loss} | {getNow()}")
+    
+    # 服务器聚合梯度并更新全局模型
+    avg_grads = server.aggregate_gradients(grads_list)
+    server.update_model(avg_grads)
 
 print("Federated learning completed.")
 
